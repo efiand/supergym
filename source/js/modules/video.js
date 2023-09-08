794efb400639285c0264398424f5f85d523fd8e7
Andrey Efimov <efiand@ya.ru>
2023-09-08 15:18:55 +0300

    fix universal videoplayer

--- source/js/modules/video.js
+++ source/js/modules/video.js
@@ -1,37 +1,57 @@
 import loadScript from '../utils/load-script.js';
 
+const CHECKING_INTERVAL = 100;
 let scriptLoaded = false;
+let playerIsReady = false;
 
 export default (containerElement) => {
   const linkElement = containerElement.querySelector('[data-control]');
   const youtubeElement = containerElement.querySelector('[data-youtube]');
   let youtubePlayer = null;
 
-  // Делаем кнопку из ссылки
-  const controlElement = document.createElement('button');
-  controlElement.className = linkElement.className;
-  controlElement.setAttribute('style', linkElement.getAttribute('style'));
-  controlElement.innerHTML = linkElement.innerHTML;
-  containerElement.replaceChild(controlElement, linkElement);
+  const createPlayer = () => {
+    youtubePlayer = new YT.Player(youtubeElement, {
+      events: {
+        onStateChange(event) {
+          if (event.data === 0) {
+            containerElement.classList.remove('is-active');
+          }
+        },
+      },
+      videoId: youtubeElement.dataset.youtube,
+    });
 
-  controlElement.addEventListener('click', () => {
-    containerElement.classList.add('is-active');
+    // Делаем кнопку из ссылки
+    const controlElement = document.createElement('button');
+    controlElement.className = linkElement.className;
+    controlElement.innerHTML = linkElement.innerHTML;
+    const styles = linkElement.getAttribute('style');
+    if (styles) {
+      controlElement.setAttribute('style', styles);
+    }
 
-    youtubePlayer.playVideo();
-  });
+    let interval = null;
+    const checkPlayer = () => {
+      if (typeof youtubePlayer.playVideo === 'function') {
+        clearInterval(interval);
+        youtubePlayer.playVideo();
+      }
+    };
+    controlElement.addEventListener('click', () => {
+      containerElement.classList.add('is-active');
+      interval = setInterval(checkPlayer, CHECKING_INTERVAL);
+    });
+    containerElement.replaceChild(controlElement, linkElement);
+  };
 
   const initPlayer = () => {
+    if (playerIsReady) {
+      createPlayer();
+      return;
+    }
     window.onYouTubePlayerAPIReady = () => {
-      youtubePlayer = new YT.Player(youtubeElement.id, {
-        events: {
-          onStateChange(event) {
-            if (event.data === 0) {
-              containerElement.classList.remove('is-active');
-            }
-          },
-        },
-        videoId: youtubeElement.id,
-      });
+      createPlayer();
+      playerIsReady = true;
     };
   };
 
